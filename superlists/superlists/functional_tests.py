85b3c8f06d7fc5649c168f7804b9f0dad0518928
Seunghun Lee <waydi1@gmail.com>
2017-11-23 23:56:05 +0900

    Add item_text to list

--- superlists/superlists/functional_tests.py
+++ superlists/superlists/functional_tests.py
@@ -33,10 +33,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = self.browser.find_elements_by_tag_name('tr')
 
-        self.assertTrue(
-                any(row.text == '1: Buy feacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy feacock feathers', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
