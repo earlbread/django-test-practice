fdd924d5ca63e451dc68d462a16f7c59a5b8bc1b
Seunghun Lee <waydi1@gmail.com>
2017-11-26 00:03:47 +0900

    Move test to the function

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -14,21 +14,23 @@ class HomePageTest(TestCase):
 
 
 class ItemModelTest(TestCase):
-    first_item_text = 'The first (ever) list item'
-    first_item = Item()
-    first_item.text = first_item_text
-    first_item.save()
 
-    second_item_text = 'Item the second'
-    second_item = Item()
-    second_item.text = second_item_text
-    second_item.save()
+    def test_saving_and_retrieving_items(self):
+        first_item_text = 'The first (ever) list item'
+        first_item = Item()
+        first_item.text = first_item_text
+        first_item.save()
 
-    saved_items = Item.objects.all()
-    self.assertEqual(saved_items.count(), 2)
+        second_item_text = 'Item the second'
+        second_item = Item()
+        second_item.text = second_item_text
+        second_item.save()
 
-    first_saved_item = saved_items[0]
-    second_saved_item = saved_items[1]
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-    self.assertEqual(first_saved_item.text, first_item_text)
-    self.assertEqual(second_saved_item.text, second_item_text)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, first_item_text)
+        self.assertEqual(second_saved_item.text, second_item_text)
