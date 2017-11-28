70075dd1283af98108baaf92c8248f38a2c797f5
Seunghun Lee <waydi1@gmail.com>
2017-11-28 09:15:15 +0900

    Remove voodoo waits

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,10 +1,14 @@
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
 
 import time
 import unittest
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -19,6 +23,21 @@ class NewVisitorTest(LiveServerTestCase):
         rows = self.browser.find_elements_by_tag_name('tr')
 
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = self.browser.find_elements_by_tag_name('tr')
+
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise(e)
+                time.sleep(0.1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -37,15 +56,15 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy feacock feathers')
+        self.wait_for_row_in_list_table('1: Buy feacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use feacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy feacock feathers')
-        self.check_for_row_in_list_table('2: Use feacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy feacock feathers')
+        self.wait_for_row_in_list_table('2: Use feacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
