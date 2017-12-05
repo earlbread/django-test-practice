4a4cb3f0696b3855b919a87ace7633bf7f356982
Seunghun Lee <waydi1@gmail.com>
2017-12-05 23:13:12 +0900

    Add a unique URL for new list

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='list_view'),
+    url(r'^lists/(.+)/$', views.view_list, name='list_view'),
 ]
